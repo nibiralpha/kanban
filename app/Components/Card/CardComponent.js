5402ce70be85570ad1ba834cc414623b2c291ca7
Nibir Alam <nibiralpha@gmail.com>
2026-04-07 17:52:13 +0600

    card component created

---
+++ app/Components/Card/CardComponent.js
@@ -0,0 +1,25 @@
+"use client";
+
+import styles from "./Card.module.css";
+
+export default function CardComponent() {
+  return (
+    <div className={styles.inner_column}>
+      <div className={styles.header}>
+        <div className={styles.backlog}>Backlog</div>
+        <div className={styles.plus}>+</div>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.title_area}>
+          <div className={styles.title}>ID - 01</div>
+          <div className={styles.priority}>
+            <div className={styles.priority_dot}></div>
+            <div className={styles.priority_text}>HIGH</div>
+          </div>
+        </div>
+        <div className={styles.card_title}>Design & Development</div>
+        <div className={styles.card_desc}>Impliment CRUD on users</div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,12 @@
+import CardComponent from "./Components/Card/CardComponent";
+
 export default function Home() {
   return (
     <div className="container">
       <div className="top_margin">
         <div className="row">
           <div className="col-3">
-            <div className="inner-column">
-              <div className="header">
-                <div className="backlog">Backlog</div>
-                <div className="plus">+</div>
-              </div>
-              <div className="card">
-                <div className="title_area">
-                  <div className="title">ID - 01</div>
-                  <div className="priority">
-                    <div className="priority_dot"></div>
-                    <div className="priority_text">HIGH</div>
-                  </div>
-                </div>
-                <div className="card_title">Design & Development</div>
-                <div className="card_desc">Impliment CRUD on users</div>
-              </div>
-            </div>
+            <CardComponent />
           </div>
         </div>
       </div>
