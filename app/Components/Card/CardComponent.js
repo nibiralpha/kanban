9bc1f465ebaa135a3ad3b045a1c2080986df1ccd
Nibir Alam <nibiralpha@gmail.com>
2026-04-14 20:23:04 +0600

    menu added

--- app/Components/Card/CardComponent.js
+++ app/Components/Card/CardComponent.js
@@ -1,17 +1,26 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import styles from "./Card.module.css";
 
-export default function CardComponent({ data, type, deleteTask, editTaskById }) {
+export default function CardComponent({
+  data,
+  type,
+  deleteTask,
+  editTaskById,
+}) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const deleteTaskById = (id) => {
     deleteTask(id);
+    setIsMenuOpen(false);
   };
 
   return (
     <div className={styles.inner_column}>
       <div className={styles.card}>
         <div className={styles.title_area}>
-          <div className={styles.title}>ID - 01</div>
+          <div className={styles.title}>ID - 1</div>
           <div className={styles.priority}>
             <div
               className={
@@ -39,22 +48,73 @@ export default function CardComponent({ data, type, deleteTask, editTaskById }) 
             </div>
           </div>
         </div>
+
         <div className={styles.card_title}>{data?.title}</div>
         <div className={styles.card_desc}>{data?.desc}</div>
 
-        <div className="mt-5 flex">
-          <div
-            onClick={() => editTaskById(data.id)}
-            className="text-[#0002ff] mr-2 text-sm cursor-pointer"
-          >
-            Edit
+        <div className="mt-5 flex justify-between relative">
+          {" "}
+          <div className="flex">
+            <div
+              onClick={() => editTaskById(data.id)}
+              className="text-[#0002ff] mr-2 text-sm cursor-pointer"
+            >
+              Edit
+            </div>
+            <div className="text-[#d4d4d4] text-sm">|</div>
+            <div
+              className="text-[#ff0000] ml-2 text-sm cursor-pointer"
+              onClick={() => deleteTaskById(data.id)}
+            >
+              Delete
+            </div>
           </div>
-          <div className="text-[#d4d4d4] text-sm">|</div>
-          <div
-            className="text-[#ff0000] ml-2 text-sm cursor-pointer"
-            onClick={() => deleteTaskById(data.id)}
-          >
-            Delete
+          <div className="relative">
+            <div
+              className="menu cursor-pointer"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+              <img src={"/menu.png"} width="20px" alt="menu" />
+            </div>
+
+            {isMenuOpen && (
+              <div className="absolute right-0 bottom-8 w-32 bg-white rounded shadow-lg z-10 p-2">
+                <ul className="text-sm">
+                  <li
+                    className="p-2 hover:bg-gray-100 cursor-pointer"
+                    onClick={() => {
+                      setIsMenuOpen(false);
+                    }}
+                  >
+                    Move to Backlog
+                  </li>
+                  <li
+                    className="p-2 hover:bg-gray-100 cursor-pointer"
+                    onClick={() => {
+                      setIsMenuOpen(false);
+                    }}
+                  >
+                    Move to Todo
+                  </li>
+                  <li
+                    className="p-2 hover:bg-gray-100 cursor-pointer"
+                    onClick={() => {
+                      setIsMenuOpen(false);
+                    }}
+                  >
+                    Move to In Progress
+                  </li>
+                  <li
+                    className="p-2 hover:bg-gray-100 cursor-pointer"
+                    onClick={() => {
+                      setIsMenuOpen(false);
+                    }}
+                  >
+                    Move to Done
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </div>
